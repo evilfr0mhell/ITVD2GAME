51b6d98acb90d9e3b3fbe43478c535a3af46887a
volodchenkovaleria <valeriavolodcenko97@gmail.com>
2024-12-04 16:30:54 +0300

    я сделала рисование и стирание

--- Game.cs
+++ Game.cs
@@ -4,5 +4,14 @@ namespace game
 {
     public class Game
     {
+        public Game()
+        {
+            while (true)
+            {
+                Console.Clear();
+                //Print();
+                Console.ReadKey();
+            }
+        }
     }
 }
