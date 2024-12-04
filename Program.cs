012226feb08361512430ba2ef0732f90aa01fa32
AlisaLisa73 <zakirovaalisa07@gmail.com>
2024-12-04 16:46:50 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,34 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Выбор игрока
+        int playerChoice;
+        do
+        {
+            Console.WriteLine("Выберите дверь: 1 , 2 ");
+        } while (!int.TryParse(Console.ReadLine(), out playerChoice) || (playerChoice != 1 && playerChoice != 2));
+
+
+        // Случайный выбор врага
+        Random random = new Random();
+        int enemyAction = random.Next(1, 3); // 1 - битва, 2 - разговор
+
+        if (playerChoice == enemyAction)
+        {
+            if (playerChoice == 1)
+            {
+                Console.WriteLine("Я тебя убью!");
+
+            }
+            else
+            {
+                Console.WriteLine("Поговорим?");
+                // Здесь можно добавить разговора
+            }
+        }
+
+        Console.ReadKey();
+
         // Создание игры с передачей всех необходимых параметров в конструктор
         Game game = new Game("Варвар", 50, 100);  // Имя игрока, сила игрока, ёмкость инвентаря
 
