8a246dfd780a580400419d74fabeedf9e4d57c37
dariadub1 <lucinadaria1511@gmail.com>
2024-12-04 16:35:51 +0300

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -21,4 +21,28 @@ namespace game
             Console.WriteLine($"{Name}: {message}");
         }
     }
+
+
+    // Класс для управления диалогом между игроками
+    public class Dialogue
+    {
+        private Player player1;
+        private Player player2;
+
+        public Dialogue(Player p1, Player p2)
+        {
+            player1 = p1;
+            player2 = p2;
+        }
+
+        public void StartConversation()
+        {
+            Console.WriteLine("Игрок встретился с NPC:");
+            player1.Speak("Привет, как дела?");
+            player2.Speak("Привет! У меня всё хорошо, а у тебя?");
+            player1.Speak("Тоже хорошо, спасибо!");
+
+
+        }
+    }
 }
